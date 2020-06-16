08c9149f94c6ee9f611da7fa80e45e32ea531d1a
BuildTools <unconfigured@null.spigotmc.org>
2020-06-16 19:56:38 +0900

    add students data for test

--- src/main/java/kr/ac/ks/app/AppApplication.java
+++ src/main/java/kr/ac/ks/app/AppApplication.java
@@ -1,10 +1,26 @@
 package kr.ac.ks.app;
 
+import kr.ac.ks.app.domain.Student;
+import kr.ac.ks.app.repository.LessonRepository;
+import kr.ac.ks.app.repository.StudentRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
+
+	private final StudentRepository studentRepository;
+	private final LessonRepository lessonRepository;
+
+	public AppApplication(StudentRepository studentRepository, LessonRepository lessonRepository) {
+		this.studentRepository = studentRepository;
+		this.lessonRepository = lessonRepository;
+		for(int i = 1; i < 10; i++){
+			studentRepository.save(
+					new Student("STD_" + i , "test" + i + "@email.com")
+			);
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
